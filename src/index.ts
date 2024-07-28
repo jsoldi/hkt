921ad58f1879ce8fa8634320b351614f1f4c062e
jsoldi <jsoldi@hotmail.com>
2024-07-29 01:16:39 +0200

    total

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,20 @@
-import { array } from "./array.js";
-import { sum } from "./sum.js";
-import { chain, pipe } from "./utils.js";
-
-export * from "./hkt.js";
-export * from "./utils.js";
-export * from "./functor.js";
-export * from "./monad.js";
-export * from "./trivial.js";
-export * from "./transformer.js";
-export * from "./array.js";
-export * from "./either.js";
-export * from "./maybe.js";
-export * from "./gen.js";
-export * from "./promise.js";
-export * from "./reader.js";
-
-declare global {
-    var log: any;
-}
-
-Object.defineProperty(globalThis, 'log', {
-    set: (v) => { console.log(v); }
-});
-
-// const nums = number.multiply(array);
-
-// const one = () => nums.empty();
-// const mult = (a: number[], b: number[]): number[] => nums.append(a, b);
-// const add = (a: number[], b: number[]): number[] => nums.add(a, b);
-
-// const a = [100, 200];
-// const b = [10, 20];
-// const c = [1, 2, 3]
-
-// // test distributive property
-// log = mult(add(a, b), c); // [100, 200, 10, 20] x [1, 2, 3]
-// log = add(mult(a, c), mult(b, c)); // [100, 200] x [1, 2, 3] + [10, 20] x [1, 2, 3]
+export * from './hkt.js';
+export * from './utils.js';
+export * from './functor.js';
+export * from './monad.js';
+export * from './monadPlus.js';
+export * from './monoid.js';
+export * from './array.js';
+export * from './either.js';
+export * from './endo.js';
+export * from './gen.js';
+export * from './map.js';
+export * from './maybe.js';
+export * from './product.js';
+export * from './promise.js';
+export * from './reader.js';
+export * from './set.js';
+export * from './string.js';
+export * from './sum.js';
+export * from './transformer.js';
+export * from './trivial.js';
