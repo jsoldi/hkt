690ce0983e457842bfafe8be3cfcfb275fa58127
jsoldi <jsoldi@hotmail.com>
2025-01-21 00:57:59 +0100

    1.3.14 split array-like

--- src/trivial.ts
+++ src/trivial.ts
@@ -1,13 +1,23 @@
+import { IToArray, toArray } from "./array-like.js";
+import { KArray } from "./array.js";
 import { $I } from "./hkt.js";
 import { IMonad, monad } from "./monad.js";
 
-export type ITrivial = IMonad<$I>;
+export type ITrivial = IMonad<$I> & IToArray<KArray, $I>;
 
 export const trivial: ITrivial = (() => {
-    return {
-        ...monad<$I>({
+    const self: ITrivial = {} as ITrivial;
+
+    Object.assign(self,
+        monad<$I>({
             unit: a => a,
             bind: (a, f) => f(a)
+        }),
+        toArray<$I, $I>({
+            scalar: self,
+            toArray: a => [a],
         })
-    }
+    );
+
+    return self;
 })();
