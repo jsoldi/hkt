e3dd0cc1ee2ab71ff2067814f1bdc577b279c058
jsoldi <jsoldi@hotmail.com>
2025-02-06 23:09:32 +0100

    1.6.1 has readme tutorial

--- README.md
+++ README.md
@@ -1,3 +1,75 @@
 # @jsoldi/hkt
 
-Higher kinded types for typescript and a few utility monads.
+Lightweight higher-kinded types for TypeScript, defining basic type classes such as `Monad`, `Monoid`, and `Functor`, together with implementations for common JavaScript types such as `Array`, `Promise` and `AsyncGenerator`.
+
+## Installation
+
+```sh
+npm install @jsoldi/hkt
+```
+
+## Example
+
+```typescript
+import { array } from "./array.js";
+import { KRoot } from "./hkt.js";
+import { monad } from "./monad.js";
+import { pipe } from "./utils.js";
+
+// Non higher-kinded Log type
+export type Log<T> = [string[], T];
+
+// Higher-kinded Log type. 
+// The `$` operator can be used to pass arguments. 
+// For instance, `$<KLog, boolean>` evaluates to `Log<boolean>`
+export interface KLog extends KRoot {
+    readonly 0: unknown;
+    readonly body: Log<this[0]>;
+}
+
+// Custom monad implementation using the `KLog` type
+const logger = {
+    ...monad<KLog>({
+        unit: a => [[], a], 
+        bind: ([logA, a], f) => {
+            const [logB, b] = f(a); 
+            return [[...logA, ...logB], b]; // Concatenate logs
+        },
+    }),
+    log: <A>(log: string, a: A): Log<A> => [[log], a] // Utility function to add a log entry
+}
+
+// Normal fetch
+function fetchURL(url: string) {
+    if (url.startsWith('https://')) {
+        return `Good`;
+    } else {
+        throw new Error(`Bad`);
+    }
+}
+
+// Logging fetch
+function logFetchURL(url: string) {
+    try {
+        const data = fetchURL(url);
+        return logger.log('Success', data); // Log success
+    } catch (e: any) {
+        return logger.log(`Error`, null); // Log error
+    }
+}
+
+// `pipe` passes the first argument to the function composition of the rest
+const result = pipe(
+    logger.sequence([
+        logFetchURL('https://example/1'),
+        logFetchURL('file:///localhost/1'),
+        logFetchURL('https://example/2'),
+        logFetchURL('file:///localhost/2'),
+    ]),
+    logger.fmap(array.filter(s => s !== null)),     // Filter out null values
+    logger.fmap(array.fmap(s => s.toUpperCase()))   // Make results uppercase
+);
+
+console.log(result); // [[ 'Success', 'Error', 'Success', 'Error' ], [ 'GOOD', 'GOOD' ]]
+```
+
--- src/test.ts
+++ src/test.ts
@@ -1,17 +1,60 @@
-// import { array, KArray } from "./array.js";
-// import { async, Async, KAsync } from "./async.js";
-// import { functor, IFunctor } from "./functor.js";
-// import { $, $3, $B, $I, KRoot } from "./hkt.js";
-// import { maybe, Maybe } from "./maybe.js";
-// import { IMonad } from "./monad.js";
-// import { KPromise, promise } from "./promise.js";
-// import { trivial } from "./trivial.js";
-// import { tuple } from "./tuple.js";
+// import { array } from "./array.js";
+// import { KRoot } from "./hkt.js";
+// import { monad } from "./monad.js";
+// import { pipe } from "./utils.js";
 
-// const a = async.from([10, 20, 30, 40]);
-// const b = async.from([]);
-// const test = async.zip(b, a);
+// // Non higher-kinded Log type
+// export type Log<T> = [string[], T];
 
-// for await (const [x, y] of test()) {
-//     console.log(x, y);
+// // Higher-kinded Log type. 
+// // The `$` operator can be used to pass arguments. 
+// // For instance, `$<KLog, boolean>` evaluates to `Log<boolean>`
+// export interface KLog extends KRoot {
+//     readonly 0: unknown;
+//     readonly body: Log<this[0]>;
 // }
+
+// // Custom monad implementation using the `KLog` type
+// const logger = {
+//     ...monad<KLog>({
+//         unit: a => [[], a], // Unit function to create a Log
+//         bind: ([logA, a], f) => {
+//             const [logB, b] = f(a); // Bind function to chain Log operations
+//             return [[...logA, ...logB], b]; // Concatenate logs
+//         },
+//     }),
+//     log: <A>(log: string, a: A): Log<A> => [[log], a] // Function to create a Log with a message
+// }
+
+// // Normal fetch
+// function fetchURL(url: string) {
+//     if (url.startsWith('https://')) {
+//         return `Good`;
+//     } else {
+//         throw new Error(`Bad`);
+//     }
+// }
+
+// // Logging fetch
+// function logFetchURL(url: string) {
+//     try {
+//         const data = fetchURL(url);
+//         return logger.log('Success', data); // Log success
+//     } catch (e: any) {
+//         return logger.log(`Error`, null); // Log error
+//     }
+// }
+
+// // `pipe` passes the first argument to the function composition of the rest
+// const result = pipe(
+//     logger.sequence([
+//         logFetchURL('https://example/1'),
+//         logFetchURL('file:///localhost/1'),
+//         logFetchURL('https://example/2'),
+//         logFetchURL('file:///localhost/2'),
+//     ]),
+//     logger.fmap(array.filter(s => s !== null)),     // Filter out null values
+//     logger.fmap(array.fmap(s => s.toUpperCase()))   // Make results uppercase
+// );
+
+// console.log(result); // [[ 'Success', 'Error', 'Success', 'Error' ], [ 'GOOD', 'GOOD' ]]
